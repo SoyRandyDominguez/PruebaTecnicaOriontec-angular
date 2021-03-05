262d51294369f8fd022fe1db83d80cc91bdec456
Randy Dominguez <32862344+SoyRandyDominguez@users.noreply.github.com>
2021-03-05 02:30:12 -0400

    usando ng bootstrap para la tabla con paginacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Randy Dominguez';
+
+  page = 1;
+  pageSize = 4;
+  collectionSize = COUNTRIES.length;
+  countries: Cliente[];
+
+  constructor() {
+    this.refreshCountries();
+  }
+
+  refreshCountries() {
+    this.countries = COUNTRIES
+      .map((Cliente, i) => ({id: i + 1, ...Cliente}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+
 }
+
+
+
+interface Cliente {
+  id?: number;
+  nombre: string;
+  empresaId: number;
+  empresa: string;
+}
+
+const COUNTRIES: Cliente[] = [
+  {
+    nombre: 'Randy Dominguez',
+    empresa: 'Grupo Viamar',
+    empresaId: 1,
+    id:1
+  },
+
+];
+
